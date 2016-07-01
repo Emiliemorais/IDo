31e9256460d5a2d8b7a98f0e54d79e045b7945f3
Emiliemorais <emilie.mo@hotmail.com>
2016-07-01 11:58:32 -0300

    Adding a notification when someone solicit a bugdet

--- blog/blog_signals.py
+++ blog/blog_signals.py
@@ -3,6 +3,11 @@ from models import Message
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.contrib.auth.models import Group
 
-def my_notify(sender, instance, created, **kwargs):
+def message_notify(sender, instance, created, **kwargs):
 	group = Group.objects.get(name="admin")
-	notify.send(instance, recipient=group, verb=_('send a new message'))
+	notify.send(instance, recipient=group, verb=_('send a new message'), description='new message')
+
+def budget_notify(sender, instance, created, **kwargs):
+	group = Group.objects.get(name="admin")
+	notify.send(instance, recipient=group, verb=_('solicited a budget'), description='new solicited budget')
+
